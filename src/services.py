d4de73892f892702a727af1c2e7dc1e2ba563b77
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-09-28 16:01:21 +0300

    update: services
    added EPİAŞ TGT service

--- src/services.py
+++ src/services.py
@@ -1,1 +1,33 @@
+import os
+from dotenv import load_dotenv
+import requests
+from datetime import date, timedelta
 
+class EpiasTransparencyerServices:
+    def __init__(self):
+        self.main_url = ""
+
+    def tgt(self):
+        """
+        Get TGT for authentication
+        """
+        load_dotenv()
+
+        resp = requests.post(
+            "https://giris.epias.com.tr/cas/v1/tickets",
+            headers={
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Accept": "application/json"
+                },
+            data={
+                "username": os.getenv("USERNAME_"),
+                "password": os.getenv("PASSWORD_")
+            }
+            )
+        return resp
+
+    def grt(self):
+        pass
+
+    def info_powerplant():
+        pass
--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,6 @@
+import os
+import json
 
+def save_json(data, path):
+    with open(path, "w") as f:
+        f.write(json.dumps(data, indent=4))
